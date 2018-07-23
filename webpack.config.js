b134397d0bc7ae571f54cff4f5ec75fe66adc941
Derya Akyol <deryaakyol@Deryas-iMac.local>
2018-07-23 06:15:02 +0100

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const htmlWebpackPlugin = new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    });
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  plugins: [htmlWebpackPlugin],
+  resolve: {
+    extensions: [".js", ".jsx"]
+  }
+};
